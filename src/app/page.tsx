a916c5573d8d2b121fdbfcde8f5e984dd397acd7
Satyam Raj <118035430+enggsatyamraj@users.noreply.github.com>
2024-06-14 21:54:02 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>
+    <div className="h-[100vh] w-[100vw] flex flex-col items-center justify-between">
       <h1 className='font-bold text-5xl'>Powerlifting Club Of Bihar</h1>
       <h3 className='font-semibold text-3xl opacity-80'>Website is Under Development, Come back soon</h3>
     </div>
